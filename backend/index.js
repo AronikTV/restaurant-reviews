74e1c5bf6809b3dcb3bae592ae70d141776b4c93
AronikTV <68695323+AronikTV@users.noreply.github.com>
2022-07-11 20:40:25 +0200

    RestaurantsController and Api Router

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 
 dotenv.config()
 
@@ -19,6 +20,7 @@ MongoClient.connect(
     })
 
     .then(async client => {
+        await RestaurantsDAO.injectDB(client)
         app.listen(port, () => {
             console.log('Server is listening on port ' + port)
         })
