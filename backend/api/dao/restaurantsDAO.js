4b00e0fa722125efa48dc60ad30d59e2b9455ef2
AronikTV <68695323+AronikTV@users.noreply.github.com>
2022-07-11 20:14:16 +0200

    Rewrote some buggy code

--- backend/api/dao/restaurantsDAO.js
+++ backend/api/dao/restaurantsDAO.js
@@ -1,3 +1,5 @@
+import { query } from "express"
+
 let restaurants
 
 export default class RestuarantsDAO {
@@ -19,15 +21,40 @@ static async getRestaurants({
     filters = null,
     page = 0,
     restaurantsPerPage = 20,
-} = {}) {
+  } = {}) {
     let query
     if (filters) {
-        if ("name" in filters) {
-            query = { $text: { $search: filters["name"] } }
-        } else if ("cuisine" in filters) {
-            query = { cuisine: filters["cuisine"] } }
-        } else if ("zipcode" in filters) {
-            query = { "address.zipcode": { $eq: filters["zipcode!"] } }
-        }
+      if ("name" in filters) {
+        query = { $text: { $search: filters["name"] } }
+      } else if ("cuisine" in filters) {
+        query = { "cuisine": { $eq: filters["cuisine"] } }
+      } else if ("zipcode" in filters) {
+        query = { "address.zipcode": { $eq: filters["zipcode"] } }
+      }
     }
-}
+
+    let cursor
+
+    try {
+        cursor = await restaurants
+        .find(query)
+    } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { restaurantsList: [], totalNumRestaurants: 0 }
+    }
+
+    const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
+
+    try {
+        const restaurantsList = await displayCursor.toArray()
+        const totalNumRestaurants = await restaurants.countDocuments(query)
+  
+        return { restaurantsList, totalNumRestaurants }
+      } catch (e) {
+        console.error(
+          `Unable to convert cursor to array or problem counting documents, ${e}`,
+        )
+        return { restaurantsList: [], totalNumRestaurants: 0 }
+      }
+      
+} 
